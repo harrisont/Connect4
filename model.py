20d0d6ec36762393cdb660bd9dee0432f91b5b0e
Harrison Ting <runhappy@gmail.com>
2013-11-28 03:56:42 -0500

    Add ability to get and set pieces on the board
    Note that the setting pieces on the board currently always puts them on
    the bottom row.

--- model.py
+++ model.py
@@ -1,3 +1,8 @@
+class Piece:
+    none = 0
+    player1 = 1
+    player2 = 2
+
 class Model:
     _SIZE_X = 7
     _SIZE_Y = 6
@@ -5,6 +10,42 @@ class Model:
     def __init__(self):
         self.size_x = self._SIZE_X
         self.size_y = self._SIZE_Y
+        self._initialize_board()
+
+    def _initialize_board(self):
+        self._openings = [[Piece.none for y in range(self.size_y)] for x in range(self.size_x)]
+
+    def get_piece_at_opening(self, x, y):
+        """
+        >>> m = Model()
+        >>> m.get_piece_at_opening(0, 0)
+        0
+        >>> m.get_piece_at_opening(m.size_x - 1, m.size_y - 1)
+        0
+        >>> m.get_piece_at_opening(-1, -1)
+        Traceback (most recent call last):
+        ValueError: Invalid position
+        >>> m.get_piece_at_opening(m.size_x - 1, m.size_y)
+        Traceback (most recent call last):
+        ValueError: Invalid position
+        """
+        if not (0 <= x < self.size_x and 0 <= y < self.size_y):
+            raise ValueError('Invalid position')
+        return self._openings[x][y]
+
+    def drop_piece(self, piece, x):
+        """
+        @param piece (Piece)
+
+        >>> m = Model()
+        >>> m.get_piece_at_opening(2, 0)
+        0
+        >>> m.drop_piece(Piece.player1, 2)
+        >>> m.get_piece_at_opening(2, 0)
+        1
+        """
+        # TODO: actually drop the piece
+        self._openings[x][0] = piece
 
 def run_tests():
     """
