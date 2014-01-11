0e01d2be774d5c8db89f8665663b5f5c7ef6b7a3
Harrison Ting <runhappy@gmail.com>
2014-01-11 14:58:32 -0800

    Simplify the MainMenuView padding
    Also calculate the menu width based on the font size.

--- main_menu_view.py
+++ main_menu_view.py
@@ -3,11 +3,11 @@ import main_menu_model
 import pygame
 
 class MainMenuView:
-    _BACKGROUND_COLOR = pygame.Color(0, 0, 0, 220)
+    _BACKGROUND_COLOR = pygame.Color(0, 0, 0, 210)
     _SELECTION_COLOR = pygame.Color(255, 255, 255)
     _POSITION = (100, 100)
-    _ENTRY_TEXT_PADDING = 5
-    _FONT_SIZE = 36
+    _ENTRY_TEXT_PADDING_X = 15
+    _FONT_SIZE = 72
     _FONT_COLOR = pygame.Color(255, 255, 255)
     _FONT_HOVER_COLOR = pygame.Color(0, 0, 0)
 
@@ -16,6 +16,15 @@ class MainMenuView:
 
         pygame.init()
         self._font = pygame.font.Font(None, self._FONT_SIZE)
+
+        self._size_x = self._calculate_size_x()
+
+    def _calculate_size_x(self):
+        max_entry_size_x = 0
+        for entry in self._model.entries:
+            entry_size_x, entry_size_y = self._font.size(entry.text)
+            max_entry_size_x = max(max_entry_size_x, entry_size_x)
+        return max_entry_size_x + 2*self._ENTRY_TEXT_PADDING_X
 
     def draw(self, screen):
         background_surface = self._create_background()
@@ -26,8 +35,7 @@ class MainMenuView:
         screen.blit(background_surface, self._POSITION)
 
     def _create_background(self):
-        size = (200,
-                len(self._model.entries) * (self._FONT_SIZE + self._ENTRY_TEXT_PADDING) - self._ENTRY_TEXT_PADDING)
+        size = (self._size_x, len(self._model.entries) * self._get_entry_height())
         surface = pygame.Surface(size)
         surface.fill(self._BACKGROUND_COLOR)
         surface.set_alpha(self._BACKGROUND_COLOR.a)
@@ -45,22 +53,23 @@ class MainMenuView:
             message_surface = self._font.render(entry.text, True, color)
             screen.blit(message_surface, position)
 
-    def _get_entry_position(self, entry_index):
-        return (0,
-                entry_index * (self._FONT_SIZE + self._ENTRY_TEXT_PADDING))
-
     def _get_entry_text_position(self, entry_index):
         entry_position_x, entry_position_y = self._get_entry_position(entry_index)
-        return (entry_position_x + self._ENTRY_TEXT_PADDING,
-                entry_position_y + self._ENTRY_TEXT_PADDING)
+        return (entry_position_x + self._ENTRY_TEXT_PADDING_X,
+                entry_position_y + 0.1*self._font.get_linesize())
 
     def _draw_selection(self, screen):
-        size = (200, self._FONT_SIZE)
+        size = (self._size_x, self._get_entry_height())
         surface = pygame.Surface(size)
         surface.fill(self._SELECTION_COLOR)
-
         position = self._get_entry_position(self._model.current_index)
         screen.blit(surface, position)
+
+    def _get_entry_position(self, entry_index):
+        return (0, entry_index * self._get_entry_height())
+
+    def _get_entry_height(self):
+        return 1.1*self._font.get_linesize()
 
 def run_tests():
     """
