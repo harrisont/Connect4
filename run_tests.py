1645a36871aec463a36de92efe1e11a72f6fad0d
Harrison Ting <runhappy@gmail.com>
2017-07-04 14:17:58 -0700

    Change run_tests.py to exit with failure on failed tests

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,13 @@
+import sys
+
 import connect4
 
 
-connect4.run_tests()
+def _main():
+    (failure_count, test_count), tested_modules = connect4.run_tests()
+    if failure_count:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    _main()
